49827a72befce5f592d0e757cd03d92e4029c922
Lorel <lorel.oboue@gmail.com>
2022-08-02 12:31:44 +0100

    basic ui complete

--- meme-app/src/App.js
+++ meme-app/src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">😂Random Meme Generator!🤣</header>
+
+      <div className="generate-button">
+        <button>Generate</button>
+      </div>
+
+      <footer>
+        <h2>😆Click for laughs😁</h2>
+      </footer>
     </div>
   );
 }
 
 export default App;
+
+// have a saave to dashboard feature, a ... or dropdown section that allows you to save to dashboard or device...
+
+// when on dashboard page, a link to take you to home
+// when on home the title is the home link and another link to take you to dashboard
+
+//addtodashboard comp
+// generatorbutton comp - with onclick, when click it fetches the random meme
